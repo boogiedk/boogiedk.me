794cf426be233a7ae0e1a7ac7248f1c1b6736f12
Кулькин Дмитрий Вячеславович <dvkulkin@mts.ru>
2023-03-20 21:08:58 +0300

    fix resume

--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -6,96 +6,95 @@ function About() {
         <div className="container">
             <header>
                 <div className="photo">
-                    <img src="https://via.placeholder.com/150" alt="Фото"/>
-                </div>
+                    <img src="https://via.placeholder.com/150" alt="Photo"/>
+                    <div className="contacts">
+                    <p>Telegram: <a href="https://t.me/boogiedk" target="_blank">@boogiedk</a></p>
+                    <p>E-mail: boogiedkjob@gmail.com</p>
+                    <p>Github: <a href="https://github.com/boogiedk" target="_blank">github.com/boogiedk</a></p>
+                    </div>
+                    </div>
                 <div className="header-info">
                     <h1>Kulkin Dmitry</h1>
-                    <p>Software Developer</p>
-                    <p>Tg: @boogiedk</p>
-                    <p>Email: boogiedkjob@gmail.com</p>
+                    <p>Software Engineer</p>
+                    <p>25 years old</p>
+                    <p>Russia, Moscow</p>
+                <div className="description">
+                   <p>I’m a software developer with 4+ years of experience in commercial software development.
+                  Microservices, SRE, build application architecture and web application development.</p>
+                </div>
                 </div>
             </header>
 
-            <hr/>
-            
-                <section className="section">
-                    <div className="section-heading">
-                        <h2 className="section-title">Experience</h2>
-                    </div>
-                    <ul>
-                        <li>
-                            <h3>MTS Digital</h3>
-                            <p>Moscow, moskva.mts.ru/</p>
-                            <p>Information technology, systems integration, internet</p>
-                            <p>Development of software</p>
-                            <p>Middle Backend .Net Developer</p>
-                            <p>Served as a lead backend developer in the MasterHub team, where we created an information system for subscribing to our own MTS services and partner services such as Spotify, Youtube, Tinder, and OZON. The project consisted entirely of microservices (about 30) on Net Core 3.1 - Net 5.0. My contributions included implementing features such as basic authentication for sending notifications to partners, developing mutually exclusive groups for the convergent service, and optimizing SQL queries in Dapper. Additionally, I resolved technical debt, conducted deadlock investigations, and launched a device installment purchase service. Our technology stack includes ELK, Docker, K8S, RabbitMQ, MS SQL, and Dapper.</p>
-                        </li>
-                        <li>
-                            <h3>TOPCASE</h3>
-                            <p>Moscow, www.itopcase.ru/</p>
-                            <p>Information technology, systems integration, internet</p>
-                            <p>Systems integration, automation of technological and business processes of enterprises, IT consulting</p>
-                            <p>Middle FullStack .Net Developer</p>
-                            <p>Worked on the Supplier Portal project aimed at automating the activities of participants in the placement of state orders and certain categories of legal entities and individuals in concluding transactions. The project was built using .Net Core 2.2 - 3.1, Ef Core/NHibernate, React + Typescript, Hangfire, MS SQL, ElasticSearch, and RabbitMQ/MassTransit. My tasks included creating functionality for administrators and moderators of the portal, and I made significant contributions to the Integration Journal development. From March 2020, I worked remotely.</p>
-                        </li>
-                        <li>
-                            <h3>IML</h3>
-                            <p>Moscow, iml.ru/</p>
-                            <p>Transportation, logistics, warehouse, foreign economic activity</p>
-                            <p>Courier, postal delivery</p>
-                            <p>Junior .Net Developer</p>
-                            <p>Developed and implemented new functionality for the fiscalization service and made contributions to the courier workplace project. Also briefly worked with WebForms, maintained the mobile application written in Java, and developed and optimized internal API services. From March 2020, I worked remotely.</p>
-                        </li>
-                    </ul>
-                </section>
+            <hr className={"hr-sections"}/>
 
-            <hr/>
-
-            <section>
-                <h2>Skills</h2>
+            <section className="section">
+                <div className="section-heading">
+                    <h2 className="section-title">Experience</h2>
+                </div>
                 <ul>
-                    <li>.NET Core 2.2 - 3.1</li>
-                    <li>Entity Framework Core/NHibernate</li>
-                    <li>React + Typescript</li>
-                    <li>Dapper</li>
-                    <li>MS SQL</li>
-                    <li>ElasticSearch</li>
-                    <li>RabbitMQ/MassTransit</li>
-                    <li>Docker/K8S</li>
-                    <li>ELK</li>
-                    <li>SignalR</li>
+                    <li>
+                        <div>
+                            <h3>MTS Digital, September 2021 to until now</h3>
+                            <p><b>Location:</b> Moscow, moskva.mts.ru/</p>
+                            <p><b>Domain:</b> Information technology, systems integration, internet</p>
+                            <p><b>Job title:</b> Senior Backend Developer</p>
+                            <br/>
+                            <p>Served as a lead backend developer in the MasterHub team, where we created an information system for subscribing to our own MTS services and partner services such as Spotify, Youtube, Tinder, and OZON. The project consisted entirely of microservices (about 30) on Net Core 3.1 - Net 5.0. My contributions included implementing features such as basic authentication for sending notifications to partners, developing mutually exclusive groups for the convergent service,
+                                and
+                                optimizing SQL queries in Dapper. Additionally, I resolved technical debt, conducted deadlock investigations, and launched a device installment purchase service. Our technology stack includes ELK, Docker, K8S, RabbitMQ, MS SQL, and Dapper.</p>
+                        </div>
+                    </li>
+                    <hr className={"hr-experience"}/>
+                    <li>
+                        <h3>TOPCASE, October 2019 to August 2022</h3>
+                        <p><b>Location:</b> Moscow, www.itopcase.ru/</p>
+                        <p><b>Domain:</b> Systems integration, automation of technological and business processes of enterprises, IT consulting</p>
+                        <p><b>Job title:</b> Middle FullStack Developer</p>
+                        <br/>
+                        <p>Worked on the Supplier Portal project aimed at automating the activities of participants in the placement of state orders and certain categories of legal entities and individuals in concluding transactions. The project was built using .Net Core 2.2 - 3.1, Ef Core/NHibernate, React + Typescript, Hangfire, MS SQL, ElasticSearch, and RabbitMQ/MassTransit. My tasks included creating functionality for administrators and moderators of the portal, and I made significant
+                            contributions to the Integration Journal development. From March 2020, I worked remotely.</p>
+                    </li>
+                    <hr className={"hr-experience"}/>
+                    <li>
+                        <h3>IML, July 2018 to Febrary 2020</h3>
+                        <p><b>Location:</b> Moscow, iml.ru/</p>
+                        <p><b>Domain:</b> Transportation, logistics, warehouse, foreign economic activity, Courier, postal delivery</p>
+                        <p><b>Job title:</b> Junior Fullstack Developer</p>
+                        <br/>
+                        <p>Developed and implemented new functionality for the fiscalization service and made contributions to the courier workplace project. Also briefly worked with WebForms, maintained the mobile application written in Java, and developed and optimized internal API services. From March 2020, I worked remotely.</p>
+                    </li>
                 </ul>
             </section>
 
-            <hr/>
+            <hr className={"hr-sections"}/>
 
             <div className="section">
                 <h2>Education</h2>
                 <ul>
                     <li>
-                        <strong>Master's degree in Information Systems and Technologies</strong><br/>
-                        Russian Technological University, Moscow (2022)<br/>
-                        Institute of Complex Security and Special Instrumentation
+                        <b>Degree:</b> Master's degree in Information Systems and Technologies, 2020-2022<br/>
+                        <b>University:</b> Russian Technological University, Moscow <br/>
+                        <b>Departament:</b> Institute of Complex Security and Special Instrumentation
                     </li>
+                    <hr className={"hr-experience"}/>
                     <li>
-                        <strong>Bachelor's degree in Information Security</strong><br/>
-                        Russian Technological University, Moscow (2020)<br/>
-                        Institute of Complex Security and Special Instrumentation
+                        <b>Degree:</b> Bachelor's degree in Information Security, 2016-2020<br/>
+                       <b>University:</b> Russian Technological University, Moscow <br/>
+                       <b>Departament:</b> Institute of Complex Security and Special Instrumentation
                     </li>
                 </ul>
             </div>
 
-            <hr/>
+            <hr className={"hr-sections"}/>
 
             <section>
-                <h2>Contacts</h2>
+                <h2>Other resources</h2>
                 <ul>
                     <li>
-                        <p>Telegram: @boogiedk</p>
+                        <p>Habr: <a href="https://habr.com/ru/users/boogiedk" target="_blank">Dmitry K. @boogiedk</a></p>
                     </li>
                     <li>
-                        <p>Email: boogiedkjob@gmail.com</p>
+                        <p>LeetCode: <a href="https://leetcode.com/" target="_blank">boogiedk</a></p>
                     </li>
                 </ul>
             </section>
